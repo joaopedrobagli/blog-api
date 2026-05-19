d9a9d56bf46ceac72a599c5eae55b5d9df473b7d
joaopedrobagli <joaobagli.pedro1506@gmail.com>
2026-05-19 16:08:12 -0300

    adicionando página de apresentação da API

--- blog-api/src/main/java/com/joaobagli/blog_api/config/SecurityConfig.java
+++ blog-api/src/main/java/com/joaobagli/blog_api/config/SecurityConfig.java
@@ -60,7 +60,7 @@ public class SecurityConfig {
         http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                .requestMatchers("/api/auth/**", "/swagger-ui/**", "/v3/api-docs/**", "/", "/index.html").permitAll()
                 .anyRequest().authenticated()
             )
             .sessionManagement(session -> session

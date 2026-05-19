71728f1b16102b4425faad80a8f89466699529fc
joaopedrobagli <joaobagli.pedro1506@gmail.com>
2026-05-19 12:28:10 -0300

    feat: adicioando JPA repositories

---
+++ blog-api/src/main/java/com/joaobagli/blog_api/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.joaobagli.blog_api.repository;
+
+import com.joaobagli.blogapi.entity.Post;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+    List<Post> findByAuthorId(Long authorId);
+}
